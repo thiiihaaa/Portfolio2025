2bf7b020f2a34d3bb9bfd4661be4dc63a547245e
thiiihaaa <veb01-wi240011@sankogakuen.jp>
2025-04-28 20:24:21 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector(".item1").addEventListener("click", function(){
+document.querySelector(".contact").addEventListener("click", function(){
     document.querySelector(".overlay").style.display = "block";
     document.querySelector(".popup").style.display = "block";
 })
@@ -7,3 +7,29 @@ document.querySelector(".closeBtn").addEventListener("click",function(){
     document.querySelector(".overlay").style.display = "none";
     document.querySelector(".popup").style.display = "none";
 })
+document.querySelector(".overlay").addEventListener("click", function() {
+    document.querySelector(".overlay").style.display = "none";
+    document.querySelector(".popup").style.display = "none";
+});
+//end of pop up1
+
+
+
+
+document.querySelector(".item1").addEventListener("click", function(){
+    document.querySelector(".overlay1").style.display = "block";
+    document.querySelector(".popup1").style.display = "block";
+})
+document.querySelector(".closeBtn1").addEventListener("click",function(){
+    document.querySelector(".overlay1").style.display = "none";
+    document.querySelector(".popup1").style.display = "none";
+})
+document.querySelector(".overlay1").addEventListener("click", function() {
+    document.querySelector(".overlay1").style.display = "none";
+    document.querySelector(".popup1").style.display = "none";
+});
+// end of popup2
+
+
+
+
